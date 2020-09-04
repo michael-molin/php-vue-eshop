d045a91abb53427ab371166059ffb07128ca22a5
Michael <m.molin.1987@gmail.com>
2020-09-04 16:33:11 +0200

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -34,7 +34,7 @@ Vue.prototype.$userId = document.querySelector("meta[name='user_id']").getAttrib
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
- */
+ * /
 
 const app = new Vue({
     store,
